7d42cb9229e2a3796b354383d06a0396d6c387c2
Instructor79 <instructor79vk@googlemail.com>
2018-04-30 12:56:41 +0100

    refactor: refactored RecipesLoader
    - added license to RecipesLoader

--- app/src/main/java/vkurman/bakingapp/loaders/RecipesLoader.java
+++ app/src/main/java/vkurman/bakingapp/loaders/RecipesLoader.java
@@ -1,3 +1,19 @@
+/*
+* Copyright (C) 2018 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package vkurman.bakingapp.loaders;
 
 import android.content.Context;
@@ -14,11 +30,10 @@ import vkurman.bakingapp.utils.JsonUtils;
 import vkurman.bakingapp.utils.RecipeUtils;
 
 /**
- * RecipesLoader
+ * RecipesLoader loads recipes from web source using RecipeUtils.
  * Created by Vassili Kurman on 07/04/2018.
  * Version 1.0
  */
-
 public class RecipesLoader extends AsyncTaskLoader<List<Recipe>> {
 
     private static final String TAG = "RecipesLoader";
