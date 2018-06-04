0ce7aefb4327055a20ab22f3d9b41967e038b0af
Instructor79 <instructor79vk@googlemail.com>
2018-06-04 13:00:48 +0100

    refactor: disabled hiding action and status bar in full screen
    - disabled hiding action and status bar in full screen due to unpleasant ui view

--- app/src/main/java/vkurman/bakingapp/ui/MediaPlayerFragment.java
+++ app/src/main/java/vkurman/bakingapp/ui/MediaPlayerFragment.java
@@ -160,16 +160,16 @@ public class MediaPlayerFragment extends Fragment implements Player.EventListene
             if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
                 if (getActivity() != null) {
                     // Hide action and status bar
-                    View decorView = getActivity().getWindow().getDecorView();
-                    decorView.setSystemUiVisibility(
-                            // Set the content to appear under the system bars so that the
-                            // content doesn't resize when the system bars hide and show.
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                    // Hide the nav bar and status bar
-                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                    | View.SYSTEM_UI_FLAG_FULLSCREEN);
+//                    View decorView = getActivity().getWindow().getDecorView();
+//                    decorView.setSystemUiVisibility(
+//                            // Set the content to appear under the system bars so that the
+//                            // content doesn't resize when the system bars hide and show.
+//                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                                    // Hide the nav bar and status bar
+//                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                                    | View.SYSTEM_UI_FLAG_FULLSCREEN);
                     // Set the player view size same as the screen size
                     DisplayMetrics displayMetrics = new DisplayMetrics();
                     WindowManager windowManager = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
